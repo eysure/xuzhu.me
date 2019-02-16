25a99ccfa1355face00e161648950da0982c13d5
Eysure <eysure@gmail.com>
2019-02-16 11:33:07 -0600

    Test

---
+++ src/pages/about.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "gatsby";
+
+import Layout from "../components/layout";
+import Image from "../components/image";
+import SEO from "../components/seo";
+
+export default () => (
+    <Layout>
+        <SEO title="About" keywords={[`gatsby`, `application`, `react`]} />
+        <p>Hi, this is the about page</p>
+        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+            <Image />
+        </div>
+        <Link to="/">Go Home</Link>
+    </Layout>
+);
